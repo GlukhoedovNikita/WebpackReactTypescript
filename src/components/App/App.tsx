d77f4e52fb1dd307e2926a6bb5d9b6af5f122cf2
Glukhoedov Nikita <nikita.runikita.ru@gmail.com>
2022-07-23 04:21:11 +0300

    1.0.0

---
+++ src/components/App/App.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react'
+
+import heart from '@assets/heart.svg'
+
+import styles from './App.module.scss'
+
+const App: FC = () => (
+    <div className={styles.Container}>
+        App
+        <img
+            width={70}
+            height={70}
+            src={heart}
+            alt=""
+        />
+    </div>
+)
+
+export default App
